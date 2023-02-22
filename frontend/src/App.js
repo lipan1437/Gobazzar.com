4f371f7cd85e13bf39c2c864cc4dba5c35ee9170
Rittik Haldar <rickhaldar30@gmail.com>
2023-02-22 16:13:32 +0530

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import { Popular } from './Components/HomePage/popular';
+import { BuySell } from './Components/HomePage/buy&sell';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Popular/>
+     <BuySell />
     </div>
   );
 }
