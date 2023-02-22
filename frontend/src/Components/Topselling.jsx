12f6f20a9829e0d8b008b1f7d9f3647a9dd48b70
Lipan Padhan <lipanpadhan1432@gmail.com>
2023-02-22 17:11:57 +0530

    adding topselling page

--- frontend/src/Components/Topselling.jsx
+++ frontend/src/Components/Topselling.jsx
@@ -5,71 +5,75 @@ const Topselling = () => {
   return (
     <div className="main">
       <div className="container">
-        <div className="inside-container1">
-          <div>
-            <h1>Top Selling Brands</h1>
-          </div>
-          <div className="but">
-            <button>See All</button>
-          </div>
-        </div>
-        <div className="inside-container2">
-          <div>
-            <img
-              src="https://trademarks.justia.com/media/image.php?serial=77976400"
-              alt="iphone_logo"
-            />
-          </div>
-          <div>
-            <img
-              src="https://bl-i.thgim.com/public/incoming/k2uo2t/article66267522.ece/alternates/FREE_1200/samsung.JPG"
-              alt="samsung_logo"
-            />
-          </div>
-          <div>
-            <img
-              src="https://images.wuzzuf-data.net/files/company_logo/Nokia-Egypt-14672.png"
-              alt="nokia_logo"
-            />
-          </div>
-
-          <div>
-            <img
-              src="https://suntecproject.s3.amazonaws.com/BI/merchant/sponsore/mobile_large/9603-Authorised-MI-Store.jpg"
-              alt="mi_logo"
-            />
-          </div>
-
-          <div>
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5ia0mK5i1-1FD1O_hbxzTYqlFGDzrnncDtudrQeM_Kw&s"
-              alt="motorola_logo"
-            />
-          </div>
-          <div>
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoQ0UC8nE_QN1m7J1iwQWJ3gE7bktKDmxH-qxJIF0aHpg-48sCHkBIFPFRul_9aPTf8tg&usqp=CAU"
-              alt="oneplus_logo"
-            />
-          </div>
-        </div>
-
-        <div className="inside-container3">
-          <div>
-            <h1>Search For Mobile Phones</h1>
-          </div>
+        <div className="shadow">
           <div className="inside-container1">
             <div>
-              <input type="text" placeholder="brand name" />
+              <h1>Top Selling Brands</h1>
+            </div>
+            <div className="but">
+              <button>See All</button>
+            </div>
+          </div>
+          <div className="inside-container2">
+            <div>
+              <img
+                src="https://trademarks.justia.com/media/image.php?serial=77976400"
+                alt="iphone_logo"
+              />
             </div>
             <div>
-              <input type="text" placeholder="model name" />
+              <img
+                src="https://bl-i.thgim.com/public/incoming/k2uo2t/article66267522.ece/alternates/FREE_1200/samsung.JPG"
+                alt="samsung_logo"
+              />
             </div>
             <div>
-              <input type="text" placeholder="price range" />
+              <img
+                src="https://images.wuzzuf-data.net/files/company_logo/Nokia-Egypt-14672.png"
+                alt="nokia_logo"
+              />
             </div>
+
+            <div>
+              <img
+                src="https://suntecproject.s3.amazonaws.com/BI/merchant/sponsore/mobile_large/9603-Authorised-MI-Store.jpg"
+                alt="mi_logo"
+              />
+            </div>
+
+            <div>
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5ia0mK5i1-1FD1O_hbxzTYqlFGDzrnncDtudrQeM_Kw&s"
+                alt="motorola_logo"
+              />
+            </div>
+            <div>
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoQ0UC8nE_QN1m7J1iwQWJ3gE7bktKDmxH-qxJIF0aHpg-48sCHkBIFPFRul_9aPTf8tg&usqp=CAU"
+                alt="oneplus_logo"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="shadow">
+          <div className="inside-container3">
+            <div>
+              <h1>Search For Mobile Phones</h1>
+            </div>
+            <div className="inside-container1">
+              <div>
+                <input type="text" placeholder="brand name" />
+              </div>
+              <div>
+                <input type="text" placeholder="model name" />
+              </div>
+              <div>
+                <input type="text" placeholder="price range" />
+              </div>
               <div className="search">
-              <button>Search Mobile</button>
+                <button>Search Mobile</button>
+              </div>
             </div>
           </div>
         </div>
@@ -95,6 +99,31 @@ const Topselling = () => {
               />
             </div>
           </Carousel>
+        </div>
+
+        <div className="shadow">
+          <div className="inside-container3">
+            <div>
+              <h1>Find Mobile by Budget</h1>
+            </div>
+            <div className="inside-container1">
+              <div  className="buget">
+                <button>Below ₹ 6,000</button>
+              </div>
+              <div className="buget">
+                <button> ₹ 6,001 - ₹ 10,000</button>
+              </div>
+              <div className="buget">
+                <button> ₹ 10,001 - ₹ 15,000</button>
+              </div>
+              <div className="buget">
+                <button> Above ₹ 15,000</button>
+              </div>
+              <div className="but">
+                <button>See All</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
