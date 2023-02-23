64cdfbe4ce913f7c1e1b6e21a4a6627e09da8141
Mdarifali912 <mdarifali912@gmail.com>
2023-02-23 21:56:51 +0530

    frontend

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -22,7 +22,7 @@ import {
         bg={useColorModeValue('gray.50', 'gray.800')}>
         <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
           <Stack align={'center'}>
-            <Heading fontSize={'4xl'}>Sign in to your account</Heading>
+            <Heading fontSize={'4xl'}>Sign in to your Quikr account</Heading>
             <Text fontSize={'lg'} color={'gray.600'}>
               to enjoy all of our cool <Link color={'blue.400'}>features</Link> ✌️
             </Text>
@@ -33,12 +33,16 @@ import {
             boxShadow={'lg'}
             p={8}>
             <Stack spacing={4}>
+              <FormControl id="name">
+                <FormLabel>Enter Your Name</FormLabel>
+                <Input type="email" />
+              </FormControl>
               <FormControl id="email">
-                <FormLabel>Email address</FormLabel>
+                <FormLabel> Enter Your Email </FormLabel>
                 <Input type="email" />
               </FormControl>
               <FormControl id="password">
-                <FormLabel>Password</FormLabel>
+                <FormLabel>Enter Your Password</FormLabel>
                 <Input type="password" />
               </FormControl>
               <Stack spacing={10}>
@@ -55,7 +59,7 @@ import {
                   _hover={{
                     bg: 'blue.500',
                   }}>
-                  Sign in
+                  Log in
                 </Button>
               </Stack>
             </Stack>
