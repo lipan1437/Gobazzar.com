1a8a55717b8d51ee501840db64a2d1cc0e0ecb05
Sejal Jaiswal <710sejal@gmail.com>
2023-02-26 13:30:21 +0530

    main

--- frontend/src/Components/HomePage/trandingadd.jsx
+++ frontend/src/Components/HomePage/trandingadd.jsx
@@ -56,23 +56,23 @@ import "../css/trending.css"
             infinite: false,
             speed: 500,
             slidesToShow: 4,
-            slidesToScroll: 4,
+            slidesToScroll: 1,
             initialSlide: 0,
             responsive: [
               {
                 breakpoint: 1024,
                 settings: {
                   slidesToShow: 3,
-                  slidesToScroll: 3,
+                  slidesToScroll: 1,
                   infinite: true,
-                  dots: true,
+                  dots: false,
                 },
               },
               {
                 breakpoint: 600,
                 settings: {
                   slidesToShow: 2,
-                  slidesToScroll: 2,
+                  slidesToScroll: 1,
                   initialSlide: 2,
                 },
               },
